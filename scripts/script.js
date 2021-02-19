ea45e55e5f70507494527a2d29b0ef7f04159a5e
RainyKnight <stoltmanlucas@gmail.com>
2021-02-18 16:20:55 -0800

    Add notes about numbers

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,44 @@
-// notes
+/*
 
-// let - declare variables
-// var = declare variables (deprecated)
-// const - declare static constants
+NOTES
 
-// Capitalize Constants
-// For example:
-// const POMEGRANATE_RED = #660c21;
+let - declare variables
+var = declare variables (deprecated)
+const - declare static constants
+
+Capitalize Constants
+For example:
+    const POMEGRANATE_RED = #660c21;
+
+==
+    Compares values then spits out a boolean.
+
+    var x = 500;             
+    var y = new Number(500);
+
+    // (x == y) is true because x and y have equal values
+
+
+
+===
+
+    Compares values AND types then spits out a boolean.
+
+    var x = 500;             
+    var y = new Number(500);
+
+    // (x === y) is false because x and y have different types
+
+
+    
+!!!DON'T CREATE NUMBER OBJECTS!!!
+
+*/
 
 let text = "Hello World";
 
 console.log(text);
+
+let x = 0.2 + 0.1;
+
+alert(x);

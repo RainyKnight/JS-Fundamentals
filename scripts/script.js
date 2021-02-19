521fd97d856d919d96173344ac66b3f4ba98af11
RainyKnight <stoltmanlucas@gmail.com>
2021-02-18 16:06:25 -0800

    Add hello world and notes

---
+++ scripts/script.js
@@ -0,0 +1,13 @@
+// notes
+
+// let - declare variables
+// var = declare variables (deprecated)
+// const - declare static constants
+
+// Capitalize Constants
+// For example:
+// const POMEGRANATE_RED = #660c21;
+
+let text = "Hello World";
+
+console.log(text);
